f6d1f95d0745f5a7aad65b2d7395d2ae3c68185e
Josh Courtney <joshrcourtney@gmail.com>
2021-05-20 10:18:42 -0500

    Initial Commit

---
+++ fruits/fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruitsApp.swift
+//  fruits
+//
+//  Created by Josh Courtney on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
